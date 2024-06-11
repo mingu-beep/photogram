b18b6b3c76a5aff99a6d5f36347c0a7134275800
mingu-beep <bramurve@gmail.com>
2024-06-11 23:09:46 +0900

    add Security config
    Security Configuration 을 추가하여
    사용자 인증이 필요할 경우 로그인 페이지로 이동하도록 설계함.

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder Encode() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                // 인증을 진행할 url 지정
+                .antMatchers("/", "/user/**", "/image/**","/subscribe/**", "/comment/**").authenticated()
+                // 이외 url 에 대해서는 허가
+                .anyRequest().permitAll()
+                .and()
+                // 인증 요청이 들어올 경우에는 loginPage에 지정한 페이지로 이동시킨다.
+                .formLogin()
+                .loginPage("/auth/signin")
+                // 인증 완료시 루트페이지로 이동
+                .defaultSuccessUrl("/");
+
+        return http.build();
+    }
+}
