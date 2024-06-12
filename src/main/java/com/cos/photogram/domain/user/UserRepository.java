c600e93965cecfd5090df1f8e8154ab1c78f3686
mingu-beep <bramurve@gmail.com>
2024-06-12 23:26:49 +0900

    Photogram 회원가입 기능 구현
    1. JpaRepostory와 @Entity 애노테이션을 활용한 DB 반영 기능 구현
    2. 비밀번호 암호화 구현
    3. 회원가입 기능 완성

---
+++ src/main/java/com/cos/photogram/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.cos.photogram.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JpaRepository 에 이미 @Repository 가 포함되어있으므로 추가하지 않아도 된다.
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
